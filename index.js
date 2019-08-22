9de61bd8360a8769f7c56ff86a3b3f417286bf63
Emi Retamar <emiliano_retamar@hotmail.com.ar>
2019-08-22 15:05:36 -0300

    Probando configuracion para subir proyecto a Heroku

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev')
     urlDB = 'mongodb://localhost:27017/musify';
 else
-    urlDB = 'mongodb+srv://admin:javascript11@cluster0-oqidx.mongodb.net/test?retryWrites=true&w=majority';
+    urlDB = 'mongodb://admin:javascript11@cluster0-oqidx.mongodb.net/test?retryWrites=true&w=majority';
 
 /*
 Para eliminar el aviso de mongoose que devuelve por la consola donde hemos
